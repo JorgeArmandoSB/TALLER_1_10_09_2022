6d4eb588b8ed663ef97245192192143652266a1e
JorgeArmandoSB <jorgesilvacreativo@gmail.com>
2022-09-10 11:16:39 -0500

    respuesta taller 4 correcta

---
+++ todo_Sobre_Variables_10_09_2022/main.js
@@ -0,0 +1,10 @@
+let nombre = `Jorge`;
+let edad = 38;
+let estudiante = true;
+const curso = `JavaScripts`;
+
+let respuesta = `Nombre:  ${nombre}, edad: ${edad}, es estudiante: ${estudiante}, de que curso: ${curso}`;
+
+console.log(respuesta);
+
+
