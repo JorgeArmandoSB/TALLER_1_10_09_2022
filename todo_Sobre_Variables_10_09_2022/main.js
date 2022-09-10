0262bc815709f8e657798d5860ad0d395fcfebbf
JorgeArmandoSB <jorgesilvacreativo@gmail.com>
2022-09-10 10:35:31 -0500

    solucion taller 4 incorrecto

---
+++ todo_Sobre_Variables_10_09_2022/main.js
@@ -0,0 +1,14 @@
+// 4.	Haz un programa de nombre main.js y otro llamado index.html incluir el archivo JavaScript en el <head></ head>, que has de grabar en una carpeta llamada todo_Sobre_Variables_FechaActual, y declarar variable y asignar a cada una de ellas un valor de tipo String,Number,Booleano y mostrar los valores por medio de la consola o de la ventana emergente, Nota la variable de tipo constante const el tipo de valor es opcional
+
+
+let nombre = `Jorge`;
+let edad = 38;
+let estudiante = true;
+
+
+let respuesta = `hola `+nombre+ ` edad: ` +edad+ " " +" estudiante:"+" " +estudiante;
+
+console.log(respuesta);
+
+
+
